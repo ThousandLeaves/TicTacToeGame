9cf223b3dd3241e498e66a788837fc21e5151d68
ThousandLeaves <chris.boyd87@ymail.com>
2021-03-24 16:58:27 -0700

    Added gameSquare object

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 const moduleGameboard = (() => {
     
     let _gameGrid = [];
+    let _gridSize = 9;
 
     // Set gameboard grid when player interacts in DOM
     const setGridElems = () => {
 
     }
 
+    const addSquare = (square) => {
+        _gameGrid.push(square)
+    }
+
+    const getGridSize = () => _gridSize;
+
+    const getSquare = (id) => _gameGrid[id];
+
+    return {
+        addSquare,
+        getGridSize,
+        getSquare
+    }
+
 })();
 
-// 
+// GameSquare object, 9 total pushed to Gameboard
+const gameSquare = (id) => {
+    let _sqId = id;
+    let _symbol = "";
+
+    // Symbol is whether the square contains an X or O value
+    const getSymbol = () => _symbol;
+    const setSymbol = (sym) => {
+        _symbol = sym;
+    }
+
+    const getId = () => _sqId;
+
+    return {
+        getSymbol,
+        setSymbol,
+        getId
+    }
+};
+
+
 const gameLogic = (() => {
-    
-    const _playMove = () => {
-        alert("Testing object initialization.");
+
+    const _playMove = (i) => {
+        alert(i.classList[1]);
+        alert(moduleGameboard.getSquare(0).getId());
     }
 
     const initialize = () => {
+
+        // Set DOM objects to interact with gameSquare objects
+        // Next, push square objects to the gameGrid
         document.querySelectorAll('.game-btn').forEach(i => {
-            i.addEventListener('click', () => { _playMove() });
+            i.addEventListener('click', () => { _playMove(i) }, false);
+            let squareObject = gameSquare(i.classList[1]);
+            moduleGameboard.addSquare(squareObject);
         })
     }
 
+    // Note, module functions should be returned as an object
     return {
             initialize
            };
